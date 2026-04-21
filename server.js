9a8f5f06a947a3e2f94ab5d4aca1cfd26cb66e13
heyjeet1722 <heyjeet1722@gmail.com>
2026-04-21 20:52:15 +0530

    mysql connect

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const http = require('http');
 const os = require('os');
 const fs = require('fs');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: '192.168.7.6',
+    user: 'root',
+    password: '1234',
+    database: 'testdb'
+});
+
+connection.connect();
 
 http.createServer((req, res) => {
-    if (req.url === "/hostname") {
+    if (req.url === "/data") {
+        connection.query("SELECT 'Hello from MySQL 🚀' AS msg", (err, results) => {
+            res.end(results[0].msg);
+        });
+    } else if (req.url === "/hostname") {
         res.end(os.hostname());
     } else {
         fs.readFile("index.html", (err, data) => {
